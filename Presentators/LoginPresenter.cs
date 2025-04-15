08485202e094eb88b05a98b26d43693dc04e5c72
TobiasNicEng <englertobias125@gmail.com>
2025-04-15 17:09:23 -0300

    Initial commit

---
+++ Presentators/LoginPresenter.cs
@@ -0,0 +1,32 @@
+﻿using LoginApp.Model;
+using LoginApp.View;
+
+namespace LoginApp.Presenter
+{
+    public class LoginPresenter
+    {
+        private readonly ILoginView view;
+        private readonly UserModel userModel;
+
+        public LoginPresenter(ILoginView view)
+        {
+            this.view = view;
+            this.userModel = new UserModel();
+        }
+
+        public void ValidateLogin()
+        {
+            string username = view.Username;
+            string password = view.Password;
+
+            if (userModel.Login(username, password))
+            {
+                view.ShowMessage("Inicio de sesión exitoso.");
+            }
+            else
+            {
+                view.ShowMessage("Usuario o contraseña incorrectos.");
+            }
+        }
+    }
+}
---
+++ Views/ILoginView.cs
@@ -0,0 +1,9 @@
+﻿namespace LoginApp.View
+{
+    public interface ILoginView
+    {
+        void ShowMessage(string message);
+        string Username { get; }
+        string Password { get; }
+    }
+}
