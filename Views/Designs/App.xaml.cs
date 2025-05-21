1714635d87feb7858ce7286f3fc925b9d6df941c
Thoryx <englertobias125@gmail.com>
2025-05-21 10:30:18 -0300

    Navegation
    login sin base
    presentador iview manager menu

--- Views/Designs/App.xaml.cs
+++ Views/Designs/App.xaml.cs
@@ -1,5 +1,25 @@
+//using System;
+//using System.Windows;
+
+//namespace ProdLogApp
+//{
+//    public class Program
+//    {
+//        [STAThread]
+//        public static void Main()
+//        {
+//            App app = new App();
+//            app.Run(new Views.Login());
+//        }
+//    }
+//}
+
+
+//sin BD
 using System;
 using System.Windows;
+using ProdLogApp.Models;
+using ProdLogApp.Views;
 
 namespace ProdLogApp
 {
@@ -9,7 +29,12 @@ namespace ProdLogApp
         public static void Main()
         {
             App app = new App();
-            app.Run(new Views.Login());
+
+            // Simulación de usuario activo
+            User activeUser = new User { Dni = "Admin", IsAdmin = true };
+
+            // Iniciar la aplicación con el menú del gerente
+            app.Run(new ManagerMenu(activeUser));
         }
     }
 }
